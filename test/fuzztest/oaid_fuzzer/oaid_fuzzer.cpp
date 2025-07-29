91ef38c905e791d93691f0e8f4437ad38cf4fe76
花花 <907896134@qq.com>
2025-07-29 11:24:07 +0000

    DT FUZZ代码覆盖率优化
    Signed-off-by: 花花 <907896134@qq.com>

--- test/fuzztest/oaid_fuzzer/oaid_fuzzer.cpp
+++ test/fuzztest/oaid_fuzzer/oaid_fuzzer.cpp
@@ -24,99 +24,102 @@
 #include "accesstoken_kit.h"
 #include "nativetoken_kit.h"
 #include "token_setproc.h"
+#include "bundle_mgr_helper.h"
+#include "oaid_file_operator.h"
 #undef private
 
 using namespace std;
 using namespace OHOS::Cloud;
 
 namespace OHOS {
-const std::u16string OAID_INTERFACE_TOKEN = u"ohos.cloud.oaid.IOAIDService";
-const std::string OAID_TRACKING_CONSENT_PERMISSION = "ohos.permission.APP_TRACKING_CONSENT";
-const std::string NETWORK_PERMISSION = "ohos.permission.INTERNET";
-const std::string OAID_TRUSTLIST_EXTENSION_CONFIG_PATH = "/etc/advertising/oaid/oaid_service_config_ext.json";
+    const std::u16string OAID_INTERFACE_TOKEN = u"ohos.cloud.oaid.IOAIDService";
+    const std::string OAID_TRACKING_CONSENT_PERMISSION = "ohos.permission.APP_TRACKING_CONSENT";
+    const std::string OAID_TRUSTLIST_EXTENSION_CONFIG_PATH = "/etc/advertising/oaid/oaid_service_config_ext.json";
+    const std::string OAID_UPDATE = "/data/service/el1/public/database/oaid_service_manager/update_check.json";
 
-void AddPermission(bool isSetSystemApp, bool isGrandOaidPermission, bool isNeedRemoveConfigFile)
-{
-    if (isNeedRemoveConfigFile) {
+    bool g_isGrant = false;
+
+    void AddPermission()
+    {
         (void)remove(OAID_TRUSTLIST_EXTENSION_CONFIG_PATH.c_str());
-    }
 
-    Security::AccessToken::PermissionDef testPermDef = {
-        .permissionName = OAID_TRACKING_CONSENT_PERMISSION,
-        .bundleName = "test_oaid",
-        .grantMode = Security::AccessToken::GrantMode::USER_GRANT,
-        .availableLevel = Security::AccessToken::APL_SYSTEM_BASIC,
-        .label = "label",
-        .labelId = 1,
-        .description = "test oaid",
-        .descriptionId = 1,
-    };
+        if (!g_isGrant) {
+            Security::AccessToken::PermissionDef testPermDef = {
+                .permissionName = OAID_TRACKING_CONSENT_PERMISSION,
+                .bundleName = "test_oaid",
+                .grantMode = Security::AccessToken::GrantMode::USER_GRANT,
+                .availableLevel = Security::AccessToken::APL_SYSTEM_BASIC,
+                .label = "label",
+                .labelId = 1,
+                .description = "test oaid",
+                .descriptionId = 1,
+            };
 
-    Security::AccessToken::PermissionStateFull testState = {
-        .isGeneral = true,
-        .grantStatus = {Security::AccessToken::PermissionState::PERMISSION_GRANTED},
-        .permissionName = isGrandOaidPermission ? OAID_TRACKING_CONSENT_PERMISSION : NETWORK_PERMISSION,
-        .grantFlags = {Security::AccessToken::PermissionFlag::PERMISSION_USER_FIXED},
-        .resDeviceID = {"local"},
-    };
+            Security::AccessToken::PermissionStateFull testState = {
+                .isGeneral = true,
+                .grantStatus = {Security::AccessToken::PermissionState::PERMISSION_GRANTED},
+                .permissionName = OAID_TRACKING_CONSENT_PERMISSION,
+                .grantFlags = {Security::AccessToken::PermissionFlag::PERMISSION_USER_FIXED},
+                .resDeviceID = {"local"},
+            };
 
-    Security::AccessToken::HapInfoParams testInfoParms = {.userID = 1,
-        .bundleName = "test_oaid",
-        .instIndex = 0,
-        .appIDDesc = "test",
-        .isSystemApp = isSetSystemApp ? true : false};
+            Security::AccessToken::HapInfoParams testInfoParms = {
+                .userID = 1,
+                .bundleName = "test_oaid",
+                .instIndex = 0,
+                .appIDDesc = "test",
+                .isSystemApp = true
+            };
 
-    Security::AccessToken::HapPolicyParams testPolicyPrams = {
-        .apl = Security::AccessToken::APL_SYSTEM_BASIC,
-        .domain = "test.domain",
-        .permList = {testPermDef},
-        .permStateList = {testState},
-    };
+            Security::AccessToken::HapPolicyParams testPolicyPrams = {
+                .apl = Security::AccessToken::APL_SYSTEM_BASIC,
+                .domain = "test.domain",
+                .permList = {testPermDef},
+                .permStateList = {testState},
+            };
 
-    auto tokenID = Security::AccessToken::AccessTokenKit::AllocHapToken(testInfoParms, testPolicyPrams);
-    SetSelfTokenID(tokenID.tokenIDEx);
-}
+            auto tokenID = Security::AccessToken::AccessTokenKit::AllocHapToken(testInfoParms, testPolicyPrams);
+            SetSelfTokenID(tokenID.tokenIDEx);
 
-bool OAIDFuzzTest(const uint8_t *rawData, size_t size)
-{
-    uint32_t startCode = static_cast<uint32_t>(OHOS::Cloud::OAIDInterfaceCode::GET_OAID);
-    uint32_t endCode = static_cast<uint32_t>(OHOS::Cloud::OAIDInterfaceCode::REGISTER_CONTROL_CONFIG_OBSERVER) + 1;
-    for (uint32_t code = startCode; code <= endCode; code++) {
-        MessageParcel data;
-        data.WriteInterfaceToken(OAID_INTERFACE_TOKEN);
-        MessageParcel reply;
-        MessageOption option;
-        auto oaidService = std::make_shared<Cloud::OAIDService>();
-        if (!oaidService) {
-            return false;
+            g_isGrant = true;
         }
-        oaidService->OnRemoteRequest(code, data, reply, option);
     }
-    return true;
+
+    bool OAIDFuzzTest(const uint8_t* rawData, size_t size)
+    {
+        uint32_t startCode = static_cast<uint32_t>(OHOS::Cloud::OAIDInterfaceCode::GET_OAID);
+        uint32_t endCode = static_cast<uint32_t>(OHOS::Cloud::OAIDInterfaceCode::REGISTER_CONTROL_CONFIG_OBSERVER) + 1;
+        for (uint32_t code = startCode; code <= endCode; code++) {
+            MessageParcel data;
+            data.WriteInterfaceToken(OAID_INTERFACE_TOKEN);
+            MessageParcel reply;
+            MessageOption option;
+            auto oaidService =
+                sptr<Cloud::OAIDService>(new (std::nothrow) Cloud::OAIDService());
+            oaidService->OnRemoteRequest(code, data, reply, option);
+        }
+        // BundleMgrHelper test
+        std::vector<AppExecFwk::BundleInfo> bundleInfos;
+        AppExecFwk::BundleInfo bundleInfo;
+        bundleInfos.push_back(bundleInfo);
+        BundleMgrHelper::GetInstance()->GetBundleInfosV9ByReqPermission(bundleInfos, -1);
+        AppExecFwk::ApplicationInfo applicationInfo;
+        BundleMgrHelper::GetInstance()->GetApplicationInfoV9WithPermission("", -1, applicationInfo);
+        BundleMgrHelper::GetInstance()->ClearBundleMgrHelper();
+        // OAIDFileOperator test
+        OAIDFileOperator::IsFileExsit(OAID_UPDATE);
+        std::string destContent = "test";
+        OAIDFileOperator::OpenAndReadFile(OAID_UPDATE, destContent);
+        OAIDFileOperator::ClearFile(OAID_UPDATE);
+        return true;
+    }
 }
-}  // namespace OHOS
 
 /* Fuzzer entry point */
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
 {
     /* Run your code on data */
-    // not system app, no grand permission, no remove config file
-    OHOS::AddPermission(false, false, false);
-    OHOS::OAIDFuzzTest(data, size);
-    // system app, no grand permission, no remove config file
-    OHOS::AddPermission(true, false, false);
-    OHOS::OAIDFuzzTest(data, size);
-    // not system app, grand permission, no remove config file
-    OHOS::AddPermission(false, true, false);
-    OHOS::OAIDFuzzTest(data, size);
-    // system app, grand permission, no remove config file
-    OHOS::AddPermission(true, true, false);
-    OHOS::OAIDFuzzTest(data, size);
-    // not system app, grand permission, remove config file
-    OHOS::AddPermission(false, true, true);
-    OHOS::OAIDFuzzTest(data, size);
-    // system app, grand permission, remove config file
-    OHOS::AddPermission(true, true, true);
+    OHOS::AddPermission();
     OHOS::OAIDFuzzTest(data, size);
     return 0;
 }
